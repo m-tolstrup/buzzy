e644913afdbbd00a8f61cbda5b0aec19d733aa4e
m-tolstrup <mikjen97@gmail.com>
2023-03-07 10:15:57 +0100

    added comment

--- rust-ebpf-fuzzer/fuzz/fuzz_targets/first.rs
+++ rust-ebpf-fuzzer/fuzz/fuzz_targets/first.rs
@@ -21,16 +21,17 @@ fuzz_target!(|data: FuzzData| {
     // Backtrace environment variable for debugging.
     // env::set_var("RUST_BACKTRACE", "1");
     let output = Command::new("../ebpf-verifier/check")
-                .args(&["../data.o"])
-                .output()
-                .expect("failed to execute process");
+                 .args(&["../data.o"])
+                 .output()
+                 .expect("failed to execute process");
     
     // If no errors occur when running, unwrap stdout.
-    io::stdout().write_all(&output.stdout).unwrap();
+    // io::stdout().write_all(&output.stdout).unwrap();
     
     // If any errors occur when running, unwrap stderr instead.
-    io::stderr().write_all(&output.stderr).unwrap();
+    // io::stderr().write_all(&output.stderr).unwrap();
     
     // Status code
-    println!("output: {}", output.status);
+    // println!("output: {}", output.status);
+
 });
