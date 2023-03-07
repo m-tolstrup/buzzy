27217186b808c7bfbf9883678e886a1a2f1ee462
local <t-bsh>
2023-03-07 14:51:16 +0100

    magic byte prepend wiki + sample examples

--- rust-ebpf-fuzzer/src/elf_parser.rs
+++ rust-ebpf-fuzzer/src/elf_parser.rs
@@ -12,6 +12,21 @@ impl ElfParser {
     }
 
     pub fn parse_prog(self) -> Vec<u8>{
+        // ELF Format magic bytes for preprending FuzzData
+        // 7f = magic number, 45 4c 46 = elf
+
+        // ELF header from bpf_lxc.co
+        //7f 45 4c 46 02 01 01 00 00 00 00 00 00 00 00 00
+        //01 00 f7 00 01 00 00 00 00 00 00 00 00 00 00 00
+        //let mut elf_magic_bytes: Vec<u8> = vec![127,69,76,70,2,1,1,0,0,0,0,0,0,0,0,0,
+        //                                        1,0,247,0,1,0,0,0,0,0,0,0,0,0,0,0];
+
+        // ELF header from wiki
+        //7f 45 4c 46 02 01 01 00 00 00 00 00 00 00 00 00
+        //02 00 3e 00 01 00 00 00 c5 48 40 00 00 00 00 00
+        let mut _parsed_prog: Vec<u8> = vec![127,69,76,70,2,1,1,0,0,0,0,0,0,0,0,0,
+                                            2,0,62,0,1,0,0,0,197,72,64,0,0,0,0,0];
+
         let _parsed_prog = vec![];
 
         // Parse BpfCode to u8 here
