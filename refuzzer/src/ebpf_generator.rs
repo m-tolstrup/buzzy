a7a3833442060357ed156407f1f3174aca687d58
m-tolstrup <mikjen97@gmail.com>
2023-03-13 11:44:42 +0100

    working generator skeleton?

--- refuzzer/src/ebpf_generator.rs
+++ refuzzer/src/ebpf_generator.rs
@@ -7,6 +7,18 @@ use rbpf::insn_builder::{
     Source,
 };
 
+macro_rules! create_function_caller {
+    ($function_name:ident, $function_vector:expr) => {
+        fn $function_name (generator: &mut EbpfGenerator) {
+            for func in $function_vector {
+                func(generator);
+            }
+        }
+    };
+}
+
+create_function_caller!(call_init_zero, vec![EbpfGenerator::init_zero]);
+
 pub struct EbpfGenerator {
     seed: u32,
     prog: BpfCode,
@@ -14,35 +26,26 @@ pub struct EbpfGenerator {
     configuration: String,
 }
 
-macro_rules! create_look_ahead_function {
-    ($function_name:ident, $function_vector:tt) => {
-        fn $function_name (generator: &mut EbpfGenerator) {
-            for func in function_vector {
-                func();
-            }
-        }
-    };
-}
-
-create_function_caller!(call_init_zero, vec![EbpfGenerator.init_zero]);
-
 impl EbpfGenerator {
     pub fn new(_seed: u32) -> EbpfGenerator {
         EbpfGenerator { 
             seed: _seed,
-            pub prog: BpfCode::new(),
+            prog: BpfCode::new(),
             config_table: 42,
-            configuration: String::("InitZero"),
+            configuration: String::from("InitZero"),
         }
     }
 
     pub fn generate_program(&mut self) -> BpfCode {
 
-        match self.configuration {
-            'InitZero' => {
+        match self.configuration.as_str() {
+            "InitZero" => {
                 call_init_zero(self);
             },
-        }
+            _ => {
+                //Nothing
+            }
+        };
 
         self.prog.exit().push();
 
@@ -50,6 +53,6 @@ impl EbpfGenerator {
     }
 
     pub fn init_zero(&mut self) {
-        self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0x00).push()
+        self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0x00).push();
     }
 }
