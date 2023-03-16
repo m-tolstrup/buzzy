c03ab2f30b77a4d06d4b8cfeb367839d925265b5
Mikkel Tolstrup Jensen <68827315+m-tolstrup@users.noreply.github.com>
2023-03-16 11:32:04 +0100

    Update create_map.py

--- scripts/create_map.py
+++ scripts/create_map.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from bcc import BPF
+
+program = r"""
+    int fd;
+  
+    union bpf_attr attr = {
+        .map_type = BPF_MAP_TYPE_ARRAY;  /* mandatory */
+        .key_size = sizeof(__u32);       /* mandatory */
+        .value_size = sizeof(__u32);     /* mandatory */
+        .max_entries = 256;              /* mandatory */
+        .map_flags = BPF_F_MMAPABLE;
+        .map_name = "example_array";
+    };
+
+    fd = bpf(BPF_MAP_CREATE, &attr, sizeof(attr));
+"""
+
+b = BPF(text=program)
