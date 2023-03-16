fd44c13f3b38eee407eb3dcf1af1451825acb18b
t-bsh <t-bsh>
2023-03-16 11:17:12 +0100

    map placeholder

--- refuzzer/src/ebpf_generator.rs
+++ refuzzer/src/ebpf_generator.rs
@@ -65,12 +65,12 @@ impl EbpfGenerator<'_> {
         // TODO implement map bytecode translate in rbpf
         // https://github.com/torvalds/linux/blob/master/include/uapi/linux/bpf_common.h
         //BPF_LD_MAP_FD(BPF_REG_1, BPF_TRIAGE_MAP_FD)
-        // define BPF_LD_MAP_FD(DST, MAP_FD)
+        // define BPF_LD_MAP_FD(DST, MAP_FD) r1 = map_fd
         //     BPF_LD_IMM64_RAW(DST, BPF_PSEUDO_MAP_FD, MAP_FD)
         //         DST = 1
-        //         BPF_PSEUDO_MAP_FD = BPF_TRIAGE_MAP_FD = 1?
-        //         MAP_FD = ?
-        //         
+        //         BPF_PSEUDO_MAP_FD = BPF_TRIAGE_MAP_FD = 1
+        //         MAP_FD = ? (placeholder = set_src(1))
+        self.prog.mov(Source::Reg, Arch::X64).set_dst(1).set_src(1).push();
         //     
         //BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem)
         // define BPF_RAW_INSN(CODE, DST, SRC, OFF, IMM)
