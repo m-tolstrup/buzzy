6a460987a0ff0e97e85bebdc6b110b296d7efe78
m-tolstrup <mikjen97@gmail.com>
2023-03-27 10:16:41 +0200

    generator 0x00 to 0xff

--- refuzzer/src/ebpf_generator.rs
+++ refuzzer/src/ebpf_generator.rs
@@ -52,25 +52,29 @@ impl EbpfGenerator<'_> {
 
     pub fn init_map(&mut self) {
         // Prepare the stack for "map_lookup_elem"
-        //self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0x00).push();
+        // BPF_MOV64_IMM(BPF_REG_0, 0)
+        self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0).push();
 
         //BPF_STX_MEM(BPF_W, BPF_REG_10, BPF_REG_0, -4)
         self.prog.store_x(MemSize::Word).set_dst(10).set_src(0).set_off(-4).push();
 
+        // BPF_MOV64_REG(BPF_REG_2, BPF_REG_10)
         self.prog.mov(Source::Reg, Arch::X64).set_dst(2).set_src(10).push();
+
+        // BPF_ALU64_IMM(BPF_ADD, BPF_REG_2, -4)
         self.prog.add(Source::Imm, Arch::X64).set_dst(2).set_imm(-4).push();
 
         // Make the call to "map_lookup_elem"
-        // TODO manually insert load map bytecode*
-        // TODO implement map bytecode translate in rbpf
-        // https://github.com/torvalds/linux/blob/master/include/uapi/linux/bpf_common.h
-        //BPF_LD_MAP_FD(BPF_REG_1, BPF_TRIAGE_MAP_FD)
-        // define BPF_LD_MAP_FD(DST, MAP_FD) r1 = map_fd
-        //     BPF_LD_IMM64_RAW(DST, BPF_PSEUDO_MAP_FD, MAP_FD)
-        //         DST = 1
-        //         BPF_PSEUDO_MAP_FD = BPF_TRIAGE_MAP_FD = 1
-        //         MAP_FD = ? (placeholder = set_src(1))
-        self.prog.mov(Source::Reg, Arch::X64).set_dst(1).set_src(1).push();
+        // BPF_LD_MAP_FD(BPF_REG_1, BPF_TRIAGE_MAP_FD)
+        self.prog.mov(Source::Imm, Arch::X64).set_dst(1).set_imm(1).push();
+
+        // BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem)
+        // integer value in 'imm' field of BPF_CALL instruction selects which helper function eBPF program intends to call
+        self.prog.call().set_dst(0).set_src(0).set_off(0).set_imm(0xff_ff_ff_ff).push();
+
+        // Verify the map so that we can use it
+        // BPF_JMP_IMM(BPF_JNE, BPF_REG_0, 0, 1)
+        self.prog.jump_conditional(Cond::NotEquals, Source::Imm).set_dst(0).set_imm(1).push();
         //     
         //BPF_RAW_INSN(BPF_JMP | BPF_CALL, 0, 0, 0, BPF_FUNC_map_lookup_elem)
         // define BPF_RAW_INSN(CODE, DST, SRC, OFF, IMM)
@@ -80,16 +84,5 @@ impl EbpfGenerator<'_> {
         // integer value in 'imm' field of BPF_CALL instruction selects which helper function eBPF program intends to call
         // https://github.com/torvalds/linux/blob/master/include/uapi/linux/bpf.h#L5506
         // 1 = map_lookup_elem? 0x11_22_33_44 = 44, 33, 22, 11 (little endianness)
-        self.prog.call().set_dst(0).set_src(0).set_off(0).set_imm(0x00_00_00_00).push();
-        
-        // Verify the map so that we can use it
-        self.prog.jump_conditional(Cond::NotEquals, Source::Imm).set_dst(0).set_imm(1).push();
-        self.prog.exit().push();
-
-        // Initialize two registers by reading from map?
-        // BPF_LDX_MEM(BPF_DW, this->reg1, BPF_REG_0, 0) this->reg1?
-        self.prog.load_x(MemSize::DoubleWord).set_dst(1).set_src(0).set_off(0).push();
-        self.prog.load_x(MemSize::DoubleWord).set_dst(2).set_src(0).set_off(8).push();
-
     }
 }
