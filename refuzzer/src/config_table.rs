a4f6a7c6aab2733012797c3aaed83230323d2ed0
m-tolstrup <mikjen97@gmail.com>
2023-04-13 13:45:03 +0200

    random registers and imm value

--- refuzzer/src/config_table.rs
+++ refuzzer/src/config_table.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 #[derive(Clone, Copy)]
 pub struct ConfigTable {
 	pub seed: u32,
@@ -15,17 +17,35 @@ impl ConfigTable {
 	}
 
 	pub fn get_rand_dst_reg(self) -> u8 {
-		let reg: u8 = 0;
+		// TODO should not be completly random? Context matters
+		let reg: u8 = match rand::thread_rng().gen_range(0..6) {
+			0 => u8::from(0),
+			1 => u8::from(1),
+			2 => u8::from(2),
+			3 => u8::from(3),
+			4 => u8::from(4),
+			5 => u8::from(5),
+			_ => unreachable!(),
+		};
 		reg
 	}
 
 	pub fn get_rand_src_reg(self) -> u8 {
-		let reg: u8 = 0;
+		// TODO should not be completly random? Context matters
+		let reg: u8 = match rand::thread_rng().gen_range(0..6) {
+			0 => u8::from(0),
+			1 => u8::from(1),
+			2 => u8::from(2),
+			3 => u8::from(3),
+			4 => u8::from(4),
+			5 => u8::from(5),
+			_ => unreachable!(),
+		};
 		reg
 	}
 
 	pub fn get_rand_imm(self) -> i32 {
-		let imm: i32 = 42;
+		let imm: i32 = rand::thread_rng().gen_range(0..2147483647);
 		imm
 	}
 }
