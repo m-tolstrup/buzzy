793eacae4775f3284958672b3a6e6124a62ab7ee
m-tolstrup <mikjen97@gmail.com>
2023-04-13 13:37:06 +0200

    cool generator code B-)

--- refuzzer/src/config_table.rs
+++ refuzzer/src/config_table.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy)]
 pub struct ConfigTable {
 	pub seed: u32,
 	pub random_instr_count: u32,
@@ -11,5 +12,20 @@ impl ConfigTable {
 			// Change here if you want more or fewer instructions
 			random_instr_count: _seed & 0b111111111,
 		}
+	}
+
+	pub fn get_rand_dst_reg(self) -> u8 {
+		let reg: u8 = 0;
+		reg
+	}
+
+	pub fn get_rand_src_reg(self) -> u8 {
+		let reg: u8 = 0;
+		reg
+	}
+
+	pub fn get_rand_imm(self) -> i32 {
+		let imm: i32 = 42;
+		imm
 	}
 }
--- refuzzer/src/ebpf_generator.rs
+++ refuzzer/src/ebpf_generator.rs
@@ -39,6 +39,7 @@ impl EbpfGenerator<'_> {
             }
         };
 
+        // Always push exit instruction
         self.prog.exit().push();
 
         self.prog.clone()
@@ -46,9 +47,8 @@ impl EbpfGenerator<'_> {
 
     fn random_instructs(&mut self) {
 
-        if self.config_table.random_instr_count == 0 {
-            self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0).push();
-        }
+        // Always initialize zero - lets more program through verifier
+        self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0).push();
         
         loop {
             if self.config_table.random_instr_count == 0 {
@@ -62,9 +62,32 @@ impl EbpfGenerator<'_> {
     }
 
     fn select_random_instr(&mut self) {
-        match rand::thread_rng().gen_range(0..1) {
-            0 => self.prog.mov(Source::Imm, Arch::X64).set_dst(0).set_imm(0).push(),
-            _ => &mut self.prog,
+
+        let dst: u8 = self.config_table.get_rand_dst_reg();
+        let src: u8 = self.config_table.get_rand_src_reg();
+        let imm: i32 = self.config_table.get_rand_imm();
+
+        // Select the source type, either register or immediate
+        let source: Source = match rand::thread_rng().gen_range(0..2) {
+            0 => Source::Imm,
+            1 => Source::Reg,
+            _ => !unreachable!(),
+        };
+
+        // Choose a random instruction and set the source
+        let instruction = match rand::thread_rng().gen_range(0..4) {
+            0 => self.prog.add(source, Arch::X64).set_dst(dst),
+            1 => self.prog.sub(source, Arch::X64).set_dst(dst),
+            2 => self.prog.mul(source, Arch::X64).set_dst(dst),
+            3 => self.prog.div(source, Arch::X64).set_dst(dst),
+            _ => !unreachable!(),
+        };
+
+        // Then, depending on the source type, set the value of the source and push it
+        match source {
+            Source::Imm => instruction.set_imm(imm).push(),
+            Source::Reg => instruction.set_src(src).push(),
+            _ => !unreachable!(),
         };
     }
 
