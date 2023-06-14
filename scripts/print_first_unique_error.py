bfa993148b4f0ab75ccb79ea5636630be2cc492b
m-tolstrup <mikjen97@gmail.com>
2023-06-14 09:50:25 +0200

    updated error print script

--- scripts/print_first_unique_error.py
+++ scripts/print_first_unique_error.py
@@ -2,18 +2,19 @@ import os
 import subprocess
 
 ubpf = os.path.join("..", "ubpf", "vm")
-errors = os.path.join("..", "buzzy", "logs")
+errors = os.path.join("..", ".." "buzzy", "logs")
 
 error_dict = {}
 
-for file in os.listdir(errors):
-    if file.startswith("error") and file.endswith(".o"):
-        error_path = os.path.join("..", "..", "buzzy", "logs", file)
-        os.chdir(ubpf)
-        cmd_str = "./test " + str(error_path)
-        result = subprocess.run(cmd_str, shell=True)
+def main():
+    os.chdir(ubpf)
+    for file in os.listdir(errors):
+        if file.startswith("error") and file.endswith(".o"):
+            error_path = os.path.join(errors, file)
+            cmd_str = "./test " + str(error_path)
+            result = subprocess.run(cmd_str, shell=True)
+            if result not in error_dict:
+                error_dict[result] = str(file)
 
-        if result not in error_dict:
-            error_dict[result] = str(file)
-
-print(error_dict)
+if __name__ == "__main__":
+    print(error_dict)
