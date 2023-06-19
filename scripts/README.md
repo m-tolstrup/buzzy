e15390afe6dfc210584bf52428073fd73eda9873
m-tolstrup <mikjen97@gmail.com>
2023-06-19 11:17:21 +0200

    rough updates for translate.py

--- scripts/README.md
+++ scripts/README.md
@@ -1,12 +1,12 @@
 # Scripts
 
-Different Python scripts used to set up different eBPF utilities in the kernel.
-The scripts used to perform eBPF operations, or similar, are written using BCC; a Python package for eBPF.
-
-## Install Guide
-
-Install guide for BCC found [here](https://github.com/iovisor/bcc/blob/master/INSTALL.md).
+Different Python scripts to gather results for thesis experiments, and aggregating fuzzing results and errors.
 
 ## Scripts Overview
 
-- `translate.py` Translate ELF bytecode into a rBPF program containing a disassembler. The disassembler will print human readable instructions.
+- `translate.py` 
+- `translate.py`
+- `translate.py`
+- `translate.py`
+- `translate.py`
+- `translate.py` Translate ELF bytecode into a rBPF program containing a disassembler. The disassembler will print human readable bytecode instructions.
--- scripts/translate.py
+++ scripts/translate.py
@@ -9,60 +9,64 @@ import os
 import sys
 import subprocess
 
-if sys.argv:
-    o_path = os.path.join(*sys.argv[1].split("/"))
-    instr_count = int(sys.argv[2])
-else:
-    # Path is set here if it is not given through command line
-    o_path = os.path.join("..", "buzzy", "logs", "error1681822839119.o")
-    # Set the number of instructions to read here
-    instr_count = 4
+def main():
+    if sys.argv:
+        o_path = os.path.join(*sys.argv[1].split("/"))
+        instr_count = int(sys.argv[2])
+    else:
+        # Path is set here if it is not given through command line
+        o_path = os.path.join("..", "buzzy", "logs", "error1681822839119.o")
+        # Set the number of instructions to read here
+        instr_count = 4
 
-# The file is saved in the buzzy cargo crate
-path = os.path.join("..", "buzzy", "src", "bin", "translate.rs")
+    # The file is saved in the buzzy cargo crate
+    path = os.path.join("..", "buzzy", "src", "bin", "translate.rs")
 
-# Read bytes, but skip 64 header bytes
-try:
-    elf_bytes = b""
-    with open(o_path, 'rb') as file:
-        file.read(64)
-        # Each instruction is eight bytes
-        elf_bytes += file.read(instr_count * 8)
-except:
-    print("Failed to read from .o file")
+    # Read bytes, but skip 64 header bytes
+    try:
+        elf_bytes = b""
+        with open(o_path, 'rb') as file:
+            file.read(64)
+            # Each instruction is eight bytes
+            elf_bytes += file.read(instr_count * 8)
+    except:
+        print("Failed to read from .o file")
 
-# Rust boilerplate
-write_str = "use rbpf::disassembler;\n\n"
-write_str += "// cargo +nightly run --bin translate\n\n"
-write_str += "fn main () {\n"
-write_str += "\tlet prog = &[\n\t\t"
+    # Rust boilerplate
+    write_str = "use rbpf::disassembler;\n\n"
+    write_str += "// cargo +nightly run --bin translate\n\n"
+    write_str += "fn main () {\n"
+    write_str += "\tlet prog = &[\n\t\t"
 
-# Add the bytes from the .o file
-count = 0
-for b in elf_bytes:
-    count += 1
-    write_str += hex(b) + ", "
-    if count == 8:
-        write_str += "\n\t\t"
-        count = 0
+    # Add the bytes from the .o file
+    count = 0
+    for b in elf_bytes:
+        count += 1
+        write_str += hex(b) + ", "
+        if count == 8:
+            write_str += "\n\t\t"
+            count = 0
 
-# More boilerpalte
-write_str += "];\n\n"
-write_str += "\tdisassembler::disassemble(prog);\n"
-write_str += "}\n"
+    # More boilerpalte
+    write_str += "];\n\n"
+    write_str += "\tdisassembler::disassemble(prog);\n"
+    write_str += "}\n"
 
-# Write it to the file
-try:
-    with open(path, 'w') as file:
-        file.write(write_str)
-except:
-    print("Failed to write to .rs file")
+    # Write it to the file
+    try:
+        with open(path, 'w') as file:
+            file.write(write_str)
+    except:
+        print("Failed to write to .rs file")
 
-# Run command to run the translation
-try:
-    cmd_str = "cargo +nightly run --bin translate"
-    path = os.path.join("..", "buzzy")
-    os.chdir(path)
-    subprocess.run(cmd_str, shell=True)
-except:
-    print("Failed to run 'cargo +nightly run --bin translate'")
+    # Run command to run the translation
+    try:
+        cmd_str = "cargo +nightly run --bin translate"
+        path = os.path.join("..", "buzzy")
+        os.chdir(path)
+        subprocess.run(cmd_str, shell=True)
+    except:
+        print("Failed to run 'cargo +nightly run --bin translate'")
+
+if __name__ == "__main__":
+    main()
